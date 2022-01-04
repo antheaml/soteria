a7187c8db4a12755ef97480c813e418d67d75e09
antheaml <65175117+antheaml@users.noreply.github.com>
2022-01-04 21:01:07 +0000

    Added comments to nurse_uart_4_lcd.s

--- nurse_uart_4_lcd.s
+++ nurse_uart_4_lcd.s
@@ -2,6 +2,10 @@
     
 global  UART_Setup, UART_Transmit_Message, UART_Loop_message, UART_Transmit_Byte
 
+; **************************************************************************************
+; ----- Module which sets up UART communication required to send messages to LCD screen
+; **************************************************************************************
+
 psect	udata_acs   ; reserve data space in access ram
 UART_counter: ds    1	    ; reserve 1 byte for variable UART_counter
 
@@ -14,11 +18,14 @@ UART_Setup:
     bcf	    BRG16	; 8-bit generator only
     movlw   103		; gives 9600 Baud rate (actually 9615)
     movwf   SPBRG1, A	; set baud rate
-    bsf	    TRISC, PORTC_TX1_POSN, A	; TX1 pin is output on RC6 pin								;****
+    bsf	    TRISC, PORTC_TX1_POSN, A	; TX1 pin is output on RC6 pin								
 					; must set TRISC6 to 1
     return
 
-UART_Transmit_Message:	    ; Message stored at FSR2, length stored in W
+UART_Transmit_Message:
+    ; ****************************************************************************************
+    ; ----- Subroutine which transmits message stored at FSR2 with length stored in W via UART
+    ; ****************************************************************************************
     movwf   UART_counter, A
 UART_Loop_message:
     movf    POSTINC2, W, A
@@ -27,7 +34,10 @@ UART_Loop_message:
     bra	    UART_Loop_message
     return
 
-UART_Transmit_Byte:	    ; Transmits byte stored in W
+UART_Transmit_Byte:	    
+    ; **************************************************
+    ; ----- Subroutine which transmits byte stored in W
+    ; **************************************************
     btfss   TX1IF	    ; TX1IF is set when TXREG1 is empty
     bra	    UART_Transmit_Byte
     movwf   TXREG1, A
