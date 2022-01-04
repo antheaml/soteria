1811f452742402e53ce8108d3d95c8dba961fc69
antheaml <65175117+antheaml@users.noreply.github.com>
2022-01-04 18:27:56 +0000

    Added comments to nurse_main

--- nurse_main.s
+++ nurse_main.s
@@ -5,39 +5,46 @@ extrn	nurse_ledSetup, setup_lcd
 extrn	SPI_MasterInit, SPI_MasterTransmit, SPI_MasterRead, read_byte1, NOP_delay
 extrn	nurse_fall, nurse_alert, nurse_remote_disable
 
+; ---------- Module controlling the set-up and standby state of the nurse device ---------- ;
     
 psect	code, abs
 	
 rst:	org	0x0000	; reset vector
 	goto	nurse_setup
-
-;int_hi:	org	0x0008	; high priority interrupt
-;	goto	Nurse_Int_Hi
 	
 nurse_setup:
-    ;bsf	    TRISB, 0, A		    ; make interrupts input already done in Nurse_Interrupt_Setup 
+    ; ********************************************************
+    ; Subroutine which sets up the nurse device upon start-up
+    ; ********************************************************
     bcf	    CFGS		    ; point to Flash program memory  
     bsf	    EEPGD		    ; access Flash program memory
-    call    nurse_ledSetup	    ; set up lEDs
-    call    setup_lcd		    ; set up lcd including uart for lcd
-    ;call    Nurse_Interrupt_Setup   ; set up interrupt pins for nurse
-    ;call    Nurse_Int_Hi
-    ;call    SPI_MasterInit
-    movlw   0b00000111
-    movwf   TRISD, A  ;port d is input
+    call    nurse_ledSetup	    ; set up LEDs
+    call    setup_lcd		    ; set up LCD including UART for LCD
+    movlw   0b00000111		    ; PORTD0:2 are inputs
+    movwf   TRISD, A 
     movlw   0x00
-    movwf   TRISH, A ; port H as outputs
+    movwf   TRISH, A 		    ; PORTH0:7 are outputs
     goto    polling_main
 
     
-polling_main: ; used to be main 
-    BTFSC   PORTD, 1, A ;bit test RH7, skip if clear
+polling_main: 
+    ; ********************************************************************************************
+    ; Looping subroutine which tests the logic level of RD0:2 to determine if a fall has occured,
+    ; or the alert or disable buttons are pressed on the client device
+    ; Logic:
+    ; 		Fall occurs -> PORTD0 high -> call fall subroutine 
+    ; 		Alert button pressed -> PORTD1 high -> call alert subroutine 
+    ; 		Disable button pressed -> PORTD2 high -> call disable subroutine triggered
+    ; BTFSC = bit test f, skip if clear
+    ; Note: polling used instead of interrupt as interrupt bins (RB0:3) used by LCD screen 
+    ; ********************************************************************************************
+    BTFSC   PORTD, 1, A 
     call    nurse_alert
-    BTFSC   PORTD, 0, A ;bit test RG0, skip if clear
+    BTFSC   PORTD, 0, A 
     call    nurse_fall
-    BTFSC   PORTD, 2, A ;bit test RG2, skip if clear
+    BTFSC   PORTD, 2, A 
     call    nurse_remote_disable
-    bra     polling_main
+    bra     polling_main ; loop
     
 testing:
     call    NOP_delay
